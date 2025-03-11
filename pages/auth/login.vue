7d436907c7f0d1c1b6b70f2adede49c5d2f09d2f
Joey A. Cabelin <joey.cabelin.1@gmail.com>
2025-03-11 18:38:30 +0800

    feat: login/register interface

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -3,21 +3,30 @@
         layout: 'auth'
     })
 
-const articles = ref([1,2,3,4,5])
+    const loginInput=ref({
+    email:'',
+    password:''
+    })
 </script>
 
 <template>
-    <div class="bg-slate-100">
-        <div></div>
-        <div class="w-[300px] mt-20">
-            <div class="flex flex-col gap-2">
-                <h1 class="text-2x1">Login</h1>
-                <input type="text" name="" placeholder="E-mail" id=""
-                class="py-2 px-2 text-sm rounded-md shadow-sm focus:ring focus:ring-blue-300">
-                <input type="password" name="" placeholder="E-mail" id=""
-                class="py-2 px-2 text-sm rounded-md shadow-sm focus:ring focus:ring-blue-300">
-                <button class="rounded-md text-indigo-700 text-sm font-semibold">Login</button>
+    <div class="bg-slate-100 h-screen">
+        <div class="flex justify-between">
+            <div></div>
+            <div class="w-[300px] mt-20">
+                <div class="flex flex-col gap-2">
+                    <h1 class="text-2x1">Login</h1>
+                    <input v-model="loginInput.email" type="text" name="" placeholder="E-mail" id=""
+                    class="py-2 px-2 text-sm rounded-md shadow-sm focus:ring focus:ring-blue-300">
+                    <input v-model="loginInput.password" type="password" name="" placeholder="Password" id=""
+                    class="py-2 px-2 text-sm rounded-md shadow-sm focus:ring focus:ring-blue-300">
+                    <NuxtLink to="/auth/register" class="text-indigo-700 font-semibold">
+                        create an acount
+                    </NuxtLink>
+                    <button class="rounded-md text-white bg-indigo-700 py-2 text-sm font-semibold">Login</button>
+                </div> 
             </div>
+            <div></div>
         </div>
     </div>
 </template>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -2,13 +2,30 @@
     definePageMeta({
         layout: 'auth'
     })
-
-const articles = ref([1,2,3,4,5])
+const registerInput=ref({
+    email:'',
+    password:''
+})
 </script>
 
 <template>
-    <div>
-        <h1>register page</h1>
-        <NuxtLink to="/admin/dashboard">Move to dashboard</NuxtLink>
+    <div class="bg-slate-100 h-screen">
+        <div class="flex justify-between">
+            <div></div>
+            <div class="w-[300px] mt-20">
+                <div class="flex flex-col gap-2">
+                    <h1 class="text-2x1">Register</h1>
+                    <input v-model="registerInput.email" type="text" name="" placeholder="E-mail" id=""
+                    class="py-2 px-2 text-sm rounded-md shadow-sm focus:ring focus:ring-blue-300">
+                    <input v-model="registerInput.password" type="password" name="" placeholder="Password" id=""
+                    class="py-2 px-2 text-sm rounded-md shadow-sm focus:ring focus:ring-blue-300">
+                    <NuxtLink to="/auth/login" class="text-indigo-700 font-semibold">
+                        Login
+                    </NuxtLink>
+                    <button class="rounded-md text-white bg-indigo-700 py-2 text-sm font-semibold">Create an Acount</button>
+                </div> 
+            </div>
+            <div></div>
+        </div>
     </div>
 </template>
